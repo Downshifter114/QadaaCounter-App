8e7d975333b935db68ab32a00713c8fd3943243e
Burak GIZLICE <87895075+Downshifter114@users.noreply.github.com>
2022-10-14 16:42:12 +0300

    UI looks bwetter and project is finished!

--- app/src/main/java/com/android/example/qadaacounter/feature_qadaa/presentation/QadaaComposable.kt
+++ app/src/main/java/com/android/example/qadaacounter/feature_qadaa/presentation/QadaaComposable.kt
@@ -14,6 +14,9 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.android.example.qadaacounter.ui.theme.Purple200
+import com.android.example.qadaacounter.ui.theme.Purple500
+import com.android.example.qadaacounter.ui.theme.Purple700
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -28,30 +31,32 @@ fun Qadaa(
     viewModel: QadaaViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.value
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceAround,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        PrayerSection(id = state.fajr.id, amount = state.fajr.amount, timeStamp = state.fajr.timeStamp, onEvent = viewModel::onEvent)
-        PrayerSection(id = state.dhuhr.id, amount = state.dhuhr.amount, timeStamp = state.dhuhr.timeStamp, onEvent = viewModel::onEvent)
-        PrayerSection(id = state.asr.id, amount = state.asr.amount, timeStamp = state.asr.timeStamp, onEvent = viewModel::onEvent)
-        PrayerSection(id = state.maghrib.id, amount = state.maghrib.amount, timeStamp = state.maghrib.timeStamp, onEvent = viewModel::onEvent)
-        PrayerSection(id = state.isha.id, amount = state.isha.amount, timeStamp = state.isha.timeStamp, onEvent = viewModel::onEvent)
-        PrayerSection(id = state.vitr.id, amount = state.vitr.amount, timeStamp = state.vitr.timeStamp, onEvent = viewModel::onEvent)
+    androidx.compose.material.Surface(color = Purple700) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            PrayerSection(id = state.fajr.id, amount = state.fajr.amount, timeStamp = state.fajr.timeStamp, onEvent = viewModel::onEvent)
+            PrayerSection(id = state.dhuhr.id, amount = state.dhuhr.amount, timeStamp = state.dhuhr.timeStamp, onEvent = viewModel::onEvent)
+            PrayerSection(id = state.asr.id, amount = state.asr.amount, timeStamp = state.asr.timeStamp, onEvent = viewModel::onEvent)
+            PrayerSection(id = state.maghrib.id, amount = state.maghrib.amount, timeStamp = state.maghrib.timeStamp, onEvent = viewModel::onEvent)
+            PrayerSection(id = state.isha.id, amount = state.isha.amount, timeStamp = state.isha.timeStamp, onEvent = viewModel::onEvent)
+            PrayerSection(id = state.vitr.id, amount = state.vitr.amount, timeStamp = state.vitr.timeStamp, onEvent = viewModel::onEvent)
+        }
     }
 }
 
 @Composable
 fun PrayerSection(id: String, amount: Int, timeStamp: Long, onEvent: (QadaaEvent) -> Unit) {
-    androidx.compose.material.Surface(color = Color.LightGray, shape = RoundedCornerShape(10.dp), modifier = Modifier.padding(horizontal = 8.dp)) {
+    androidx.compose.material.Surface(color = Purple200, shape = RoundedCornerShape(10.dp), modifier = Modifier.padding(horizontal = 16.dp)) {
         Column(
             modifier = Modifier.fillMaxWidth(),
             verticalArrangement = Arrangement.SpaceAround,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Row() {
-                Text(text = "${id.uppercase()}->", textAlign = TextAlign.Center, fontSize = 24.sp)
+                Text(text = "${id.uppercase()} -> ", textAlign = TextAlign.Center, fontSize = 24.sp)
                 Text(text = "Amount: $amount", textAlign = TextAlign.Center, fontSize = 24.sp)
             }
             Text(text = "Last Edited: ${formatTimeStamp(timeStamp)}", textAlign = TextAlign.Center, fontSize = 24.sp)
